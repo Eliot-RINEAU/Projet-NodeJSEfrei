cfd132830cd1b700a7eff543e2eb976a5da2279c
Eliot-RINEAU <eliotrineau@gmail.com>
2024-11-17 23:51:47 +0100

    sqlite3

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const db = require("../database");
 
-let users = [
-    { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
-    { id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
-    { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
-    { id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
-    { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
-];
 
 // GET : LIRE tous les utilisateurs
 router.get("/", (req, res) => {
-    res.json(users);
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.json(rows);
+        }
+      });
+    
 });
 
 // GET : LIRE un utilisateur spécifique
