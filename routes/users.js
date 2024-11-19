98ae9a9cf67dfec7f3035dca4157483abaea8cbc
Eliot-RINEAU <eliotrineau@gmail.com>
2024-11-19 23:07:55 +0100

    ajout controllers

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { getAllUsers, createNewUser, updateUser, deleteUser } = require("../controllers/usersControllers");
 const db = require("../database");
 
 
+
 // GET : LIRE tous les utilisateurs
-router.get("/", (req, res) => {
-    db.all('SELECT * FROM users', [], (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(rows);
-        }
-      });
-    
-});
+router.get("/", getAllUsers);
 
 // GET : LIRE un utilisateur spécifique
 router.get("/:id", (req, res) => {
@@ -27,48 +20,12 @@ router.get("/:id", (req, res) => {
 });
 
 // POST : CRÉER un nouvel utilisateur
-router.post("/", (req, res) => {
-    const { firstName, lastName, role } = req.body;
-    const lastId = users[users.length - 1].id;
-    const newId = lastId + 1;
-    const newUser = {
-        id: newId,
-        firstName,
-        lastName,
-        role
-    };
-    users.push(newUser);
-    res.status(201).json(newUser);
-});
+router.post("/", createNewUser);
 
 // PUT : METTRE À JOUR un utilisateur
-router.put("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const userIndex = users.findIndex(user => user.id === id);
-    if (userIndex < 0) {
-        return res.status(404).json({ msg: "utilisateur non trouvé" });
-    }
-    const { firstName, lastName, role } = req.body;
-    if (firstName) users[userIndex].firstName = firstName;
-    if (lastName) users[userIndex].lastName = lastName;
-    if (role) users[userIndex].role = role;
-    res.json({
-        msg: "utilisateur mis à jour",
-        user: users[userIndex],
-    });
-});
+router.put("/:id", updateUser);
 
 // DELETE : SUPPRIMER un utilisateur
-router.delete("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const userIndex = users.findIndex(user => user.id === id);
-    if (userIndex < 0) {
-        return res.status(404).json({ msg: "utilisateur non trouvé" });
-    }
-    users.splice(userIndex, 1);
-    res.json({
-        msg: "utilisateur supprimé",
-    });
-});
+router.delete("/:id", deleteUser);
 
 module.exports = router;
